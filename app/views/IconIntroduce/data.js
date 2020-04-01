d9f521379a91106caca7753ac05992f7f49e3b2d
zhaoyu <1069080359@qq.com>
2020-04-01 17:55:27 +0800

    icon 介绍

---
+++ app/views/IconIntroduce/data.js
@@ -0,0 +1,70 @@
+export const list = [
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__profile.png'),
+    name: '基本资料',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__combined.png'),
+    name: '客户背景',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__count.png'),
+    name: '过往博彩经验',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__credit.png'),
+    name: '信贷',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__schedule.png'),
+    name: '行程',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__shoppingrecord.png'),
+    name: '购物记录',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__customer_contact.png'),
+    name: '客户接触',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__Appointment.png'),
+    name: '客户约见',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__entre.png'),
+    name: '入场动态',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_module_present_20X20select.png'),
+    name: '礼遇',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__exit.png'),
+    name: '场次分析',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__data.png'),
+    name: '户口分析',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__consume.png'),
+    name: '消费分析',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__achievement.png'),
+    name: '业绩',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__complaints.png'),
+    name: '投诉',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__demands.png'),
+    name: '需求',
+  },
+  {
+    icon: require('../../imgs/sidebarIcon/crm_rncrm_module__opinion.png'),
+    name: '意见',
+  },
+]
